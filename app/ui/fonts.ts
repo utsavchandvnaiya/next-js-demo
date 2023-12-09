3fb0e1af45f8575d0b1017b12f15ff60aa5fa7d6
Utsav <>
2023-12-09 12:13:33 +0530

    updated code

---
+++ app/ui/fonts.ts
@@ -0,0 +1,25 @@
+/*
+Why optimize fonts?
+Fonts play a significant role in the design of a website, but using custom fonts in your project can affect performance if the font files need to be fetched and loaded.
+
+Cumulative Layout Shift is a metric used by Google to evaluate the performance and user experience of a website. With fonts, layout shift happens when the browser initially renders text in a fallback or system font and then swaps it out for a custom font once it has loaded. This swap can cause the text size, spacing, or layout to change, shifting elements around it.
+
+Mock UI showing initial load of a page, followed by a layout shift as the custom font loads.
+Next.js automatically optimizes fonts in the application when you use the next/font module. It downloads font files at build time and hosts them with your other static assets. This means when a user visits your application, there are no additional network requests for fonts which would impact performance.
+
+Finally, add the font to the <body> element in /app/layout.tsx:
+
+By adding Inter to the <body> element, the font will be applied throughout your application. Here, you're also adding the Tailwind antialiased class which smooths out the font. It's not necessary to use this class, but it adds a nice touch.
+
+Navigate to your browser, open dev tools and select the body element. You should see Inter and Inter_Fallback are now applied under styles.
+
+*/
+
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
